93525ad7b650b83141f2100d2d60150ba332920b
Keoni Fontanilla <keonifontanilla@gmail.com>
2020-12-23 18:42:20 -1000

    Added recipe details

---
+++ RecipeManagerCoreMVC/Controllers/RecipesController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using RecipeManagerCoreMVC.Data;
+using RecipeManagerCoreMVC.Models;
+using RecipeManagerCoreMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManagerCoreMVC.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public RecipesController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<RecipeModel> recipes = _db.Recipes;
+
+            return View(recipes);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(HomeCreateViewModel homeCreateViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var recipeModel = homeCreateViewModel.RecipeModel;
+                var ingredientModels = homeCreateViewModel.IngredientModels;
+                var recipeIngredientsModels = homeCreateViewModel.RecipeIngredientModels;
+                var instructionModels = homeCreateViewModel.InstructionModels;
+
+                var index = 0;
+                foreach (var ingredientModel in ingredientModels)
+                {
+                    recipeIngredientsModels[index].IngredientsModel = ingredientModel;
+                    index++;
+                }
+
+                var newRecipe = new RecipeModel
+                {
+                    RecipeName = recipeModel.RecipeName,
+                    RecipeDescription = recipeModel.RecipeDescription,
+                    RecipeType = recipeModel.RecipeType,
+                    CreatedDate = recipeModel.CreatedDate,
+                    RecipeIngredientModels = recipeIngredientsModels,
+                    InstructionModels = instructionModels
+                };
+
+                _db.Add(newRecipe);
+
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+           return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            RecipeModel recipe = _db.Recipes
+                .Include(x => x.RecipeIngredientModels)
+                .ThenInclude(x => x.IngredientsModel)
+                .Include(x => x.InstructionModels)
+                .FirstOrDefault(x => x.Id == id);
+
+            return View(recipe);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
