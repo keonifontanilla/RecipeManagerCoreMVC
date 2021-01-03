d4f3fac0792589fe337944127e9eb7a74b773f8a
Keoni Fontanilla <keonifontanilla@gmail.com>
2021-01-02 22:36:05 -1000

    error logging

--- RecipeManagerCoreMVC/Controllers/ErrorController.cs
+++ RecipeManagerCoreMVC/Controllers/ErrorController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using RecipeManagerCoreMVC.Models;
 using System;
 using System.Collections.Generic;
@@ -11,12 +13,22 @@ namespace RecipeManagerCoreMVC.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
             if (statusCode == 404)
             {
                 ViewBag.ErrorMsg = "Page Not Found";
+                _logger.LogWarning($"404 error occured. Path = {statusCodeResult.OriginalPath} and QueryString = {statusCodeResult.OriginalQueryString}");
             }
             return View("NotFound");
         }
@@ -26,6 +38,10 @@ namespace RecipeManagerCoreMVC.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            _logger.LogError($"The path {exception.Path} threw and exception {exception.Error}");
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
