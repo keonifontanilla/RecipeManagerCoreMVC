4801ab965f3085ebfa1610ca67fae105f10813c4
Keoni Fontanilla <keonifontanilla@gmail.com>
2021-01-05 20:28:09 -1000

    Add, Edit, delete roles

---
+++ RecipeManagerCoreMVC/Controllers/AdministrationController.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RecipeManagerCoreMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManagerCoreMVC.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityRole = new IdentityRole { Name = createRoleViewModel.RoleName };
+                var result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded) return RedirectToAction("ListRoles", "Administration");
+
+                foreach (var identityError in result.Errors)
+                {
+                    ModelState.AddModelError("", identityError.Description);
+                }
+            }
+
+            return View(createRoleViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var editRoleViewModel = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            foreach (var user in _userManager.Users)
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    editRoleViewModel.Users.Add(user.UserName);
+                }
+            }
+
+            return View(editRoleViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel editRoleViewModel)
+        {
+            var role = await _roleManager.FindByIdAsync(editRoleViewModel.Id);
+                
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id = {editRoleViewModel.Id} cannot be found";
+                return View("NotFound");
+            }
+
+            role.Name = editRoleViewModel.RoleName;
+            var result = await _roleManager.UpdateAsync(role);
+
+            if (result.Succeeded) return RedirectToAction("ListRoles");
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(editRoleViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            var userRoleViewModels = new List<UserRoleViewModel>();
+            foreach (var user in _userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName
+                };
+
+                userRoleViewModel.IsSelected = await _userManager.IsInRoleAsync(user, role.Name) ? true : false;
+                userRoleViewModels.Add(userRoleViewModel);
+            }
+
+            return View(userRoleViewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> userRoleViewModels, string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            for (int i = 0; i < userRoleViewModels.Count; i++)
+            {
+                var user = await _userManager.FindByIdAsync(userRoleViewModels[i].UserId);
+                IdentityResult result = null;
+
+                if (userRoleViewModels[i].IsSelected && !(await _userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await _userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!userRoleViewModels[i].IsSelected && await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await _userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (result.Succeeded)
+                {
+                    if (i < (userRoleViewModels.Count - 1))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return RedirectToAction("EditRole", new { Id = roleId });
+                    }
+                }
+            }
+
+            return RedirectToAction("EditRole", new { Id = roleId });
+        }
+    }
+}
---
+++ RecipeManagerCoreMVC/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManagerCoreMVC.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; } = new List<string>();
+    }
+}
