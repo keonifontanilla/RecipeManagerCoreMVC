51681855b0a46259a1ed4441848abd6ff96ab68c
Keoni Fontanilla <keonifontanilla@gmail.com>
2021-01-21 10:29:59 -1000

    profile about me page

--- RecipeManagerCoreMVC/Controllers/ProfileController.cs
+++ RecipeManagerCoreMVC/Controllers/ProfileController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RecipeManagerCoreMVC.Data;
+using RecipeManagerCoreMVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,39 @@ namespace RecipeManagerCoreMVC.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Details(string userName)
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Details(string userName)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User {userName} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new ProfileViewModel
+            {
+                Email = user.Email,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                AboutMe = user.AboutMe,
+                Location = user.Location,
+                PinterestLink = user.PinterestLink,
+                FacebookLink = user.FacebookLink,
+                TwitterLink = user.TwitterLink
+            };
+
+            return View(model);
         }
     }
 }
---
+++ RecipeManagerCoreMVC/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RecipeManagerCoreMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManagerCoreMVC.ViewModels
+{
+    public class ProfileViewModel : ApplicationUser
+    {
+        
+    }
+}
