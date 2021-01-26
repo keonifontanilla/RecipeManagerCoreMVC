1af262ac0ccf8864751b14dc293dfff557a0cac2
Keoni Fontanilla <keonifontanilla@gmail.com>
2021-01-25 20:38:20 -1000

    view model changes

--- RecipeManagerCoreMVC/ViewModels/EditUserViewModel.cs
+++ RecipeManagerCoreMVC/ViewModels/EditUserViewModel.cs
@@ -6,27 +6,8 @@ using System.Threading.Tasks;
 
 namespace RecipeManagerCoreMVC.ViewModels
 {
-    public class EditUserViewModel
+    public class EditUserViewModel : ProfileViewModel
     {
-        public string Id { get; set; }
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
-        [Display(Name = "First name")]
-        public string FirstName { get; set; }
-        [Display(Name = "Last name")]
-        public string LastName { get; set; }
-        [Display(Name = "About me")]
-        public string AboutMe { get; set; }
-        public string Location { get; set; }
-        [Display(Name = "Pinterest")]
-        public string PinterestLink { get; set; }
-        [Display(Name = "Facebook")]
-        public string FacebookLink { get; set; }
-        [Display(Name = "Twitter")]
-        public string TwitterLink { get; set; }
+
     }
 }
