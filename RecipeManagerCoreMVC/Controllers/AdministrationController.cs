016ac2c9f863771e6e42f60ef2c101f38b96bb7a
Keoni Fontanilla <keonifontanilla@gmail.com>
2021-01-25 18:22:49 -1000

    admin edit users

--- RecipeManagerCoreMVC/Controllers/AdministrationController.cs
+++ RecipeManagerCoreMVC/Controllers/AdministrationController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using RecipeManagerCoreMVC.Data;
+using RecipeManagerCoreMVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,67 @@ namespace RecipeManagerCoreMVC.Controllers
         {
             var users = _userManager.Users;
             return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                AboutMe = user.AboutMe,
+                Location = user.Location,
+                PinterestLink = user.PinterestLink,
+                FacebookLink = user.FacebookLink,
+                TwitterLink = user.TwitterLink
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.Id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+
+            if (model.UserName != user.UserName) user.UserName = model.UserName;
+            if (model.FirstName != user.FirstName) user.FirstName = model.FirstName;
+            if (model.LastName != user.LastName) user.LastName = model.LastName;
+            if (model.AboutMe != user.AboutMe) user.AboutMe = model.AboutMe;
+            if (model.Location != user.Location) user.Location = model.Location;
+            if (model.PinterestLink != user.PinterestLink) user.PinterestLink = model.PinterestLink;
+            if (model.FacebookLink != user.FacebookLink) user.FacebookLink = model.FacebookLink;
+            if (model.TwitterLink != user.TwitterLink) user.TwitterLink = model.TwitterLink;
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded) return RedirectToAction("ListUsers");
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(model);
         }
     }
 }
